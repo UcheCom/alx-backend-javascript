6d9d2a40db7c49f4787c64f2884922a277923af5
Uchenna Oko <uche4newton1981@gmail.com>
2024-03-10 15:57:50 +0100

    Classes task 1 errors fixed

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,7 +1,11 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
+  const roomList = [];
   const roomSizes = [19, 20, 34];
-  const roomList = roomSizes.map(size => new ClassRoom(size));
+
+  for (const size of roomSizes) {
+    roomList.push(new ClassRoom(size));
+  }
   return roomList;
 }
