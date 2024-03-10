2ed32eb7126a153bab451ad0d7c1b5d0fb0994fa
Uchenna Oko <uche4newton1981@gmail.com>
2024-03-10 19:23:25 +0100

    Vroom task 10 fixed

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,23 +1,16 @@
-export default class car {
+export default class Car {
   constructor(brand, motor, color) {
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
-  get brand() {
-    return this._brand;
-  }
-
-  get motor() {
-    return this._motor;
-  }
-
-  get color() {
-    return this._color;
+  static get [Symbol.species]() {
+    return this;
   }
 
   cloneCar() {
-     return new this.constructor(this._brand, this._motor, this._color);
+    const Species = this.constructor[Symbol.species];
+    return new Species();
   }
 }
