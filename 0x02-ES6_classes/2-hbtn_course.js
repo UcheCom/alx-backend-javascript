1b983964b049dc6ac5ba1d864549b9619d6e9ffb
Uchenna Oko <uche4newton1981@gmail.com>
2024-03-10 15:46:13 +0100

    Classes task 2 errors fixed

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,5 +1,8 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof name !== 'string') throw new TypeError('Name must be a string');
+    if (!Number.isInteger(length)) throw new TypeError('Length must be a number');
+    if (!Array.isArray(students)) throw new TypeError('Students type must be an Array');
     this._name = name;
     this._length = length;
     this._students = students;
