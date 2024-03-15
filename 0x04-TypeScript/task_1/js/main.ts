d6f5c86bc6f653122672c37c7b5b2b369ddf5787
Uchenna Oko <uche4newton1981@gmail.com>
2024-03-15 09:51:26 +0100

    Creating more interfaces

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,49 @@
+interface Teacher {
+    firstName: string;
+    lastName: string;
+    readonly fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    readonly location: string;
+    [key: string]: any; // Allow any additional attribute with any name and type
+}
+
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+  
+interface PrintTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+  
+const printTeacher: PrintTeacherFunction = (firstName, lastName) => {
+    const firstLetter = firstName.charAt(0).toUpperCase();
+    const fullName = `${firstLetter}. ${lastName}`;
+    return fullName;
+};
+
+interface StudentClassConstructor {
+    new (firstName: string, lastName: string): StudentClass;
+}
+
+interface StudentClass {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements StudentClass{
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return "Currently working";
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
