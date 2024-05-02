4be103506656ea8ae12558e5d8bcbfe14425271e
Uchenna Oko <uche4newton1981@gmail.com>
2024-05-02 12:00:32 +0100

    Creating a more complex HTTP server using Express

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const { readFile } = require('fs');
+
+const PORT = 1245;
+const app = express();
+
+function countStudents(filePath) {
+  let length = 0;
+  const students = {};
+  const fields = {};
+
+  return new Promise((resolve, reject) => {
+    readFile(filePath, (error, data) => {
+      if (error) {
+        reject(error);
+      } else {
+        let output = '';
+        const lines = data.toString().split('\n');
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            length += 1;
+	    const field = lines[i].toString().split(',');
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
+            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+              fields[field[3]] += 1;
+            } else {
+              fields[field[3]] = 1;
+            }
+          }
+        }
+        const l = length - 1;
+        output += `Number of students: ${l}\n`;
+	for (const [k, v] of Object.entries(fields)) {
+          if (k !== 'field') {
+            output += `Number of students in ${k}: ${v}. `;
+            output += `List: ${students[k].join(', ')}\n`;
+          }
+        }
+        resolve(output);
+      }
+    });
+  });
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+app.get('/students', (req, res) => {
+  countStudents(process.argv[2].toString()).then((output) => {
+    res.send(['This is the list of our students', output].join('\n'));
+  }).catch(() => {
+    res.send('This is the list of our students\nCannot load the database');
+  });
+});
+
+app.listen(port, () => {
+});
+
+module.exports = app;
