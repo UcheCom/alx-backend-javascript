e35a50924f31d262149f79e155be239e21586d64
Uchenna Oko <uche4newton1981@gmail.com>
2024-05-02 11:59:13 +0100

    Creating a more complex HTTP server using Node's HTTP module

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,70 @@
+const { readFile } = require('fs');
+const http = require('http');
+
+const HOST = '127.0.0.1';
+const PORT = 1245;
+
+function countStudents(filePath) {
+  let length = 0;
+  const students = {};
+  const fields = {};
+
+  return new Promise((resolve, reject) => {
+    readFile(filePath, (error, data) => {
+      if (error) {
+        reject(error);
+      } else {
+        let output = '';
+        const lines = data.toString().split('\n');
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            length += 1;
+            const field = lines[i].toString().split(',');
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
+	    if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+              fields[field[3]] += 1;
+            } else {
+              fields[field[3]] = 1;
+            }
+          }
+        }
+	const l = length - 1;
+        output += `Number of students: ${l}\n`;
+        for (const [k, v] of Object.entries(fields)) {
+          if (k !== 'field') {
+            output += `Number of students in ${k}: ${v}. List: ${students[k].join(', ')}\n`;
+          }
+        }
+        resolve(output);
+      }
+    });
+  });
+}
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') {
+    res.write('Hello Holberton School!');
+    res.end();
+  }
+  if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    countStudents(process.argv[2].toString()).then((output) => {
+      const outString = output.slice(0, -1);
+      res.end(outString);
+    }).catch(() => {
+      res.statusCode = 404;
+      res.end('Cannot load the database');
+    });
+  }
+});
+
+app.listen(port, hostname, () => {
+});
+
+module.exports = app;
