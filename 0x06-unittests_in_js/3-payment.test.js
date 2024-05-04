fa33509ec2d2c6cf45be90b6330c4f13ba705d94
Uchenna Oko <uche4newton1981@gmail.com>
2024-05-04 17:54:23 +0100

    Adding test file

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,17 +1,18 @@
 const Utils = require('sinon');
 const sinon = require('./utils');
-const assert = require('assert');
-// const {describe, it} = require("mocha");
+// const assert = require('assert');
+const { expect } = require("chai");
 const sendPaymentRequestToApi = require('./3-payment');
 
 
 describe('sendPaymentRequestToApi', () => {
-  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
-    const spy = sinon.spy(Utils, 'calculateNumber');
+  it('uses the calculateNumber method of Utils', () => {
+    const spy = sinon.spy(Utils);
 
     sendPaymentRequestToApi(100, 20);
 
-    sinon.assert.calledWith(spy, 'SUM', 100, 20);
-    spy.restore();
+    expect(spy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    expect(spy.calculateNumber.callCount).to.be.equal(1);
+    spy.calculateNumber.restore();
   });
 });
