7a6fd095e26fcc2d9235419621df4a3378afc75a
Uchenna Oko <uche4newton1981@gmail.com>
2024-05-04 15:48:33 +0100

    Adding script files

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,19 @@
+function calculateNumber(type, a, b) {
+  if (type == 'SUM') {
+    return Math.round(a) + Math.round(b);
+  }
+  if (type == 'SUBTRACT') {
+    return Math.round(a) - Math.round(b);
+  }  
+  if (type == 'DIVIDE') {
+    const r_a = Math.round(a);
+    const r_b = Math.round(b);
+    if (r_b === 0) {
+      return 'Error';
+    }
+    return r_a / r_b;
+  }
+
+}
+
+module.exports = calculateNumber;
