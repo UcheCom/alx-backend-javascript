9ab76c2c116e85aece8d0f934b9ee820a0a1236a
Uchenna Oko <uche4newton1981@gmail.com>
2024-05-05 14:30:39 +0100

    Adding test file

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,16 @@
+const sinon = require("sinon");
+const sendPaymentRequestToApi = require("./4-payment");
+const Utils = require("./utils");
+const assert = require("assert");
+
+describe("sendPaymentRequestToApi", function() {
+    it("this checks that Utils.calculateNumber was called once", function() {
+	const stub = sinon.stub(Utils, "calculateNumber").return(10);
+	const spy = sinon.spy(console, "log");
+
+	sendPaymentRequestToApi(100, 20);
+
+	assert(spy.withArgs('The total is: 10').calledOnce);
+	assert(stub.withArgs("SUM", 100, 20).calledOnce);
+    });
+});
