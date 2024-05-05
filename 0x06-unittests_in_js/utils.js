0fdb6a4062e71c39a39725d3ead7d8601870f6ac
Uchenna Oko <uche4newton1981@gmail.com>
2024-05-05 13:55:58 +0100

    Updating utils file

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,16 +1,16 @@
 const Utils = {
   calculateNumber: function(type, a, b) {
     if (type == 'SUM') {
-    return Math.round(a) + Math.round(b);
+      return Math.round(a) + Math.round(b);
   }
   if (type == 'SUBTRACT') {
-    return Math.round(a) - Math.round(b);
+     return Math.round(a) - Math.round(b);
   }
   if (type == 'DIVIDE') {
-    const r_a = Math.round(a);
-    const r_b = Math.round(b);
-    if (r_b === 0) {
-      return 'Error';
+     const r_a = Math.round(a);
+     const r_b = Math.round(b);
+     if (r_b === 0) {
+        return 'Error';
     }
     return r_a / r_b;
    }
