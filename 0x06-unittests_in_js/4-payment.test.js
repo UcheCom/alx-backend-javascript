0987b75122616be87bcfe4595d6077b3321a32e9
Uchenna Oko <uche4newton1981@gmail.com>
2024-05-05 15:19:33 +0100

    Updating test file

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,4 +1,3 @@
-const {describe, it} = require("mocha");
 const sinon = require("sinon");
 const sendPaymentRequestToApi = require("./4-payment.js");
 const Utils = require("./utils.js");
@@ -8,8 +7,7 @@ describe("sendPaymentRequestToApi", function() {
     it("this checks that Utils.calculateNumber is stubbed", function() {
 	const stub = sinon.stub(Utils, "calculateNumber").returns(10);
 	const spy = sinon.spy(console, "log");
-
-	sendPaymentRequestToApi(100, 20);
+        sendPaymentRequestToApi(100, 20);
 
 	sinon.assert.calledWith(spy,'The total is: 10');
 	sinon.assert.calledWith(stub, "SUM", 100, 20);
