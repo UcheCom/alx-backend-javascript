8893b40212e6c808e33b75f197feb494bfad1f3e
Uchenna Oko <uche4newton1981@gmail.com>
2024-05-05 13:38:55 +0100

    Updating test file

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,16 +1,16 @@
-const Utils = require('sinon');
-const sinon = require('./utils.js');
-const assert = require('assert');
-const sendPaymentRequestToApi = require('./3-payment.js');
+const {describe, it} = require("mocha");
+const sinon = require("sinon");
+const sendPaymentRequestToApi = require("./3-payment");
+const Utils = require("./utils");
+const assert = require("assert");
 
+describe("sendPaymentRequestToApi", function() {
+    it("check that Utils.calculateNumber was called once", function() {
+	const spy = sinon.spy(Utils, "calculateNumber");
 
-describe('sendPaymentRequestToApi', function() {
-  it('uses the calculateNumber method of Utils', () => {
-    const spy = sinon.spy(Utils, calculateNumber);
+	sendPaymentRequestToApi(50, 24.52);
 
-    sendPaymentRequestToApi(100, 20);
-
-    sinon.assert.calledWith(spy, 'SUM', 100, 20);
-    spy.restore();
-  });
+	assert(spy.calledOnce);
+	spy.restore();
+    });
 });
