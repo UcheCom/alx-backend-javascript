6ae72d775e22454a5370fde29720d0b9c8001bd8
Uchenna Oko <uche4newton1981@gmail.com>
2024-05-07 16:33:06 +0100

    error corrected

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 7865;
+const port = 7865;
 
 app.use(express.json());
 
@@ -8,32 +8,25 @@ app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
-app.get('/cart/:id([0-9]+)', (req, res) => {
-    res.send(`Payment methods for cart ${request.params.id}`);
+app.get('/cart/:id(\\d+)', (req, res) => {
+  const id = req.params.id;
+  res.send(`Payment methods for cart ${id}`);
 });
 
 app.get('/available_payments', (req, res) => {
-    res.set("Content-Type", "application/json");
-    const payMethods = {
-	payment_methods: {
-          credit_cards: true,
-          paypal: false
-	}
+  res.json({
+    payment_methods: {
+      credit_cards: true,
+      paypal: false
     }
-    res.send(payMethods);
+  });
 });
 
 app.post('/login', (req, res) => {
-    const userName = req.body.userName;
-    if (userName) {
-	res.send(`Welcome ${userName}`);
-    } else {
-	res.status(404).send();
-    }
+  const userName = req.body.userName;
+  res.send(`Welcome ${userName}`);
 });
 
 app.listen(port, () => {
-    console.log(`API available on localhost port ${port}`);
+  console.log('API available on localhost port 7865');
 });
-
-module.exports = app;
