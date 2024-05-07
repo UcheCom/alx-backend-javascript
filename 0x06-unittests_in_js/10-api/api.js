74c343c03de9d1c3b235b5ab94f9402892e62b5b
Uchenna Oko <uche4newton1981@gmail.com>
2024-05-07 15:10:43 +0100

    updating Deep equality & Post integration testing

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,31 +1,39 @@
 const express = require('express');
 const app = express();
-const port = 7865;
+const PORT = 7865;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
 app.get('/cart/:id([0-9]+)', (req, res) => {
-    res.send(`Payment methods for cart ${req.params.id}`);
-});
-
-app.listen(port, () => {
-  console.log('API available on localhost port 7865');
+    res.send(`Payment methods for cart ${request.params.id}`);
 });
 
 app.get('/available_payments', (req, res) => {
-  res.json({
-    payment_methods: {
-      credit_cards: true,
-      paypal: false
+    res.set("Content-Type", "application/json");
+    const payMethods = {
+	payment_methods: {
+          credit_cards: true,
+          paypal: false
+	}
     }
-  });
+    res.send(payMethods);
 });
 
 app.post('/login', (req, res) => {
-  const userName = req.body.userName;
-  res.send(`Welcome ${userName}`);
+    const userName = req.body.userName;
+    if (userName) {
+	res.send(`Welcome ${userName}`);
+    } else {
+	res.status(404).send();
+    }
+});
+
+app.listen(port, () => {
+    console.log(`API available on localhost port ${port}`);
 });
 
 module.exports = app;
